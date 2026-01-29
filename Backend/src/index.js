e4138ded40d137edeea1adfec4fa86b672df4f35
swayam agarwal <you@example.com>
2026-01-29 20:53:02 +0530

    Fixed database issue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,36 +1,26 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
-import app from './app.js';
 
 dotenv.config({
-    path: './.env'
+  path: "./.env",
 });
 
-const startServer = async () => {
-  try {
-           await connectDB();
-
-        app.on("error", (error) => {    // to check if there are any errors 
-        console.log("ERROR", error);
-        throw error;
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
     });
-
-
-    app.listen(process.env.PORT || 8000, () => {   // app will listen
-        console.log(` Server is running at port :     
-            ${process.env.PORT}`);                 
-
-    });
-} catch (err) {
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-    
-}
-}
+  });
+
+export default app;
 
 startServer();
 
-app.get('/', (req, res) => {
-  res.send('Yo, backend is live! Welcome to the API.');
+app.get("/", (req, res) => {
+  res.send("Yo, backend is live! Welcome to the API.");
 });
-
